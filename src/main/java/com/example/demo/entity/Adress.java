9057351fd6eaed9eea62b42cf11550d19a683bb6
Cihat KOCAK <pcihatkocakks@gmail.com>
2022-10-05 19:48:59 +0300

    entitysOK

--- src/main/java/com/example/demo/entity/Adress.java
+++ src/main/java/com/example/demo/entity/Adress.java
@@ -1,5 +1,58 @@
 package com.example.demo.entity;
 
-public class Adress{
-    
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "person_adress")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+@ToString
+public class Adress implements java.io.Serializable {
+
+    @Id
+    @SequenceGenerator(name = "adress_id_seq", sequenceName = "adress_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "adress_id_seq", strategy = javax.persistence.GenerationType.SEQUENCE)
+    private Long id;
+    @Column(length = 500, name = "street")
+    private String street;
+    @Column(length = 500, name = "city")
+    private String city;
+    @Column(length = 500, name = "country")
+    private String country;
+    @Column(length = 6, name = "zip")
+    private String zipCode;
+
+    @Column(length = 50, name = "status")
+    private Boolean status;
+
+    @Enumerated
+    private AdressType adressType;
+
+    @ManyToOne
+    @JoinColumn(name = "adress_id")
+    private Person person;
+
+    enum AdressType {
+        HOME,
+        WORK,
+        OTHER
+    }
 }
---
+++ src/main/java/com/example/demo/entity/Person.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "person")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+@ToString
+public class Person {
+    @Id
+    @SequenceGenerator(name = "persons_id_seq", sequenceName = "persons_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "persons_id_seq", strategy = javax.persistence.GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(length = 100, name = "first_name")
+    private String firstName;
+
+    @Column(length = 100, name = "last_name")
+    private String lastName;
+
+    @Column(length = 11, name = "phone")
+    private String phone;
+
+    @Column(length = 50, name = "email")
+    private String email;
+
+    @OneToMany
+    @JoinColumn(name = "adress_id")
+    private List<Adress> adressList; // OneToMany -> join
+
+   
+}
